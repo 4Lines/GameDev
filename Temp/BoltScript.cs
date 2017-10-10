d1bed0fd9e89cb56829cceec016cdc80436dacf1
Keith Ng <climbr39696@gmail.com>
2017-10-10 04:25:49 -0500

    Add files via upload
    updated scripts

---
+++ Temp/BoltScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoltScript : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent <Rigidbody2D> ();
+		rb.velocity = transform.up * speed;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}
