cced23a5c1d2c2965193263e924263ddb3e28fc5
rlong713 <rlong713@yahoo.com>
2017-10-09 23:08:59 -0500

    Add files via upload
    Added mobility script with collision detection.

--- Assets/Scripts/PlayerMobility.cs
+++ Assets/Scripts/PlayerMobility.cs
@@ -9,18 +9,26 @@ public class PlayerMobility : MonoBehaviour {
 	public Transform shotSpawn;
 	public float fireRate;
     public bool moving = false;
+    public LayerMask collision;
 
+    private BoxCollider2D bc;
 	private Rigidbody2D rb;
 	private float nextFire;
 
 	// Use this for initialization
 	void Start () {
+        bc = GetComponent<BoxCollider2D>();
 		rb = GetComponent <Rigidbody2D> ();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+        if (Input.GetMouseButton(0) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation); //as GameObject;
+        }
         movement();
     }
 
@@ -30,7 +38,7 @@ public class PlayerMobility : MonoBehaviour {
         Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
 
         transform.rotation = rot;
-        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+        transform.eulerAngles = new Vector3(0, 0, shotSpawn.transform.eulerAngles.z);
         rb.angularVelocity = 0;
 
 
@@ -40,25 +48,33 @@ public class PlayerMobility : MonoBehaviour {
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.up * speed * Time.deltaTime, Space.World);
+            RaycastHit2D raycast = Physics2D.Raycast(transform.position, Vector2.up, (speed * Time.deltaTime), collision);
+            if(raycast.transform == null)
+                transform.Translate(Vector3.up * speed * Time.deltaTime, Space.World);
             moving = true;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.down * speed * Time.deltaTime, Space.World);
+            RaycastHit2D raycast = Physics2D.Raycast(transform.position, Vector2.down, (speed * Time.deltaTime), collision);
+            if (raycast.transform == null)
+                transform.Translate(Vector3.down * speed * Time.deltaTime, Space.World);
             moving = true;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+            RaycastHit2D raycast = Physics2D.Raycast(transform.position, Vector2.left, (speed * Time.deltaTime), collision);
+            if (raycast.transform == null)
+                transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
             moving = true;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+            RaycastHit2D raycast = Physics2D.Raycast(transform.position, Vector2.right, (speed * Time.deltaTime), collision);
+            if (raycast.transform == null)
+                transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
             moving = true;
         }
 
