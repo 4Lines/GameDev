fab6337bf025e639916420399fdb552f501a7307
rlong713 <rlong713@yahoo.com>
2017-10-10 01:46:47 -0500

    Add files via upload
    Updates to Environment script. Enemy behavior script created.

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+
+    public float speed;
+    public int hitPoints = 10;
+    public LayerMask collision;
+
+    private BoxCollider2D bc;
+    private Rigidbody2D rb;
+    private Transform target;
+
+    // Use this for initialization
+    void Start () {
+        bc = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    void movement()
+    {
+        int xDir = 0, yDir = 0;
+        if(Mathf.Abs(target.position.x - transform.position.x) > Mathf.Abs(target.position.y - transform.position.y))
+            xDir = target.position.x > transform.position.x ? 1 : -1;
+        else
+            yDir = target.position.y > transform.position.y ? 1 : -1;
+        if(xDir == -1)
+        {
+            RaycastHit2D raycast = Physics2D.Raycast(transform.position + (Vector3)bc.offset, Vector2.left, (speed * Time.deltaTime), collision);
+            if (raycast.transform == null)
+                transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+        }
+        else if(xDir == 1)
+        {
+            RaycastHit2D raycast = Physics2D.Raycast(transform.position + (Vector3)bc.offset, Vector2.right, (speed * Time.deltaTime), collision);
+            if (raycast.transform == null)
+                transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+        }
+        else if(yDir == -1)
+        {
+            RaycastHit2D raycast = Physics2D.Raycast(transform.position + (Vector3)bc.offset, Vector2.down, (speed * Time.deltaTime), collision);
+            if (raycast.transform == null)
+                transform.Translate(Vector3.down * speed * Time.deltaTime, Space.World);
+        }
+        else if(yDir == 1)
+        {
+            RaycastHit2D raycast = Physics2D.Raycast(transform.position + (Vector3)bc.offset, Vector2.up, (speed * Time.deltaTime), collision);
+            if (raycast.transform == null)
+                transform.Translate(Vector3.up * speed * Time.deltaTime, Space.World);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        movement();
+	}
+}
--- Assets/Scripts/Environment.cs
+++ Assets/Scripts/Environment.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Environment : MonoBehaviour {
 
     private Transform theEnvironment;
-    public int rows = 8;
-    public int columns = 8;
+    public int rows = 16;
+    public int columns = 64;
     public GameObject theFloor;
     public GameObject theWall;
 
@@ -18,7 +18,7 @@ public class Environment : MonoBehaviour {
             for(int j = -1; j < rows + 1; j++)
             {
                 GameObject toInstantiate = theFloor;
-                if (i == -1 || i == columns || j == -1 || j == rows)
+                if (i == -1 || i == columns || j == -1 || j == rows || (((i % 8) == 0) && ((j % 4) == 0)))
                     toInstantiate = theWall;
                 GameObject instance = Instantiate(toInstantiate, new Vector3(i, j, 0f), Quaternion.identity) as GameObject;
                 instance.transform.SetParent(theEnvironment);
