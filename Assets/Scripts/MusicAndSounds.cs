2d8ee53ac5d4b43b2079262133267432bbb4c0b6
rlong713 <rlong713@yahoo.com>
2017-11-07 13:38:22 -0600

    Added Music and Sounds script.

---
+++ Assets/Scripts/MusicAndSounds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicAndSounds : MonoBehaviour {
+
+    public AudioSource soundEffectSource;
+    public AudioSource musicSource;
+    public static MusicAndSounds instance = null;
+
+	// Use this for initialization
+	void Awake () {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
