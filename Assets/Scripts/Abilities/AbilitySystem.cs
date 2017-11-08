1a415e37f084e92d80a8a7d1ea1c9fc769c7071a
Keith Ng <climbr39696@gmail.com>
2017-11-07 21:22:02 -0600

    Ability System Core Script

---
+++ Assets/Scripts/Abilities/AbilitySystem.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbilitySystem : MonoBehaviour  {
+
+	private BasicObjectInfo objectInfo;
+	private List<AbilityBehaviors> behaviors;
+	private bool requiresTarget;
+	private bool CastOnSelf;
+	private int cooldown; //in sec
+	private GameObject particleEffect;
+	private float castTime;
+	private float cost;
+	private AbilityType type;
+
+	public enum AbilityType
+	{
+		Spell,
+		Melee
+	}
+
+	public AbilitySystem(BasicObjectInfo aBasicInfo)
+	{
+		objectInfo = aBasicInfo;
+		behaviors = new List<AbilityBehaviors>();
+		cooldown = 0;
+		requiresTarget = false;
+		CastOnSelf = false;
+	}
+
+	public AbilitySystem(BasicObjectInfo aBasicInfo, List<AbilityBehaviors> aBehaviors)
+	{
+		objectInfo = aBasicInfo;
+		behaviors = aBehaviors;
+		cooldown = 0;
+		requiresTarget = false;
+		CastOnSelf = false;
+	}
+
+	public AbilitySystem(BasicObjectInfo aBasicInfo, List<AbilityBehaviors> aBehaviors, 
+		bool aRequireTarget, int aCooldown, GameObject aParticleE)
+	{
+		objectInfo = aBasicInfo;
+		behaviors = new List<AbilityBehaviors>();
+		behaviors = aBehaviors;
+		cooldown = aCooldown;
+		requiresTarget = aRequireTarget ;
+		CastOnSelf = false;
+		particleEffect = aParticleE;
+	}
+
+	public BasicObjectInfo AbilityInfo 
+	{
+		get { return objectInfo; }
+	}
+
+	public int AbilityCooldown
+	{
+		get{ return cooldown;}
+	}
+
+	public List<AbilityBehaviors> AbilityBehaviors
+	{
+		get { return behaviors; }
+	}
+
+	public void UseAbility()
+	{
+		
+	}
+}
