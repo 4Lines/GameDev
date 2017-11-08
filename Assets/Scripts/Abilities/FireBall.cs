bd8124c4c2bbc8a07377fa62d0cd9c37cc299146
Keith Ng <climbr39696@gmail.com>
2017-11-07 21:18:52 -0600

    Fireball and Lightning Spell Scripts

---
+++ Assets/Scripts/Abilities/FireBall.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics ;
+using UnityEngine;
+
+public class FireBall : AbilitySystem  {
+
+	private DamageOverTime dot;
+	private const string aName = "Fire Ball";
+	private const string aDescription = "A fire spell that deals DOT";
+	// private FireBall fba;
+	// private Stopwatch abilityCooldownTimer;
+	private Rigidbody2D rb;
+	private string targetTag = "Enemy";
+
+	// public GameObject fireballPrefab;
+
+	// ability parameter 
+
+	public float baseDamage = 1f;
+	public float speed = 15f;
+
+	//DOT parameter
+	private float effectDuration = 3f;
+	private Stopwatch durationTimer = new Stopwatch();
+	private float baseEffectDamage = 0.5f;
+	private float damageTickDuration = 1f;
+
+	void Start () {
+		rb = GetComponent <Rigidbody2D> ();
+		rb.velocity = transform.up * speed;
+		dot = GetComponent <DamageOverTime> ();
+		Destroy (gameObject, 3f);
+	}
+		
+	public FireBall(): base(new BasicObjectInfo (aName, aDescription))
+	{
+		this.AbilityBehaviors.Add (new DamageOverTime (6f, baseDamage, 1f));
+	}
+
+	/*
+	public void OnAbilityuse(GameObject obj)
+	{
+		abilityCooldownTimer = new Stopwatch ();
+		abilityCooldownTimer.Start ();
+		fba = new FireBall ();
+
+		GameObject cast= Instantiate<GameObject>(fireballPrefab);
+	}
+	*/
+
+	void Update()
+	{
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag (targetTag)) {
+			other.gameObject.GetComponent<EnemyScript> ().hitPoints -= baseDamage;
+			StartCoroutine (DOT (other.gameObject));
+
+		}	
+
+	}
+
+	private IEnumerator DOT (GameObject objectHit)
+	{
+		durationTimer.Start ();
+		while (durationTimer.Elapsed.TotalSeconds <= effectDuration) {
+			objectHit.GetComponent<EnemyScript>().hitPoints -= baseEffectDamage;
+			DestroyImmediate(gameObject);
+			yield return new WaitForSeconds(damageTickDuration);
+
+		}
+
+		durationTimer.Stop ();	
+		durationTimer.Reset ();
+	
+		yield return null;
+
+	}
+}
---
+++ Assets/Scripts/Abilities/LightningSpell.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics ;
+using UnityEngine;
+
+public class LightningSpell : AbilitySystem  {
+
+	private EnemyScript enemy;
+	private Shock stun;
+	private const string aName = "Lightning Spell";
+	private const string aDescription = "A lightning spell stuns an object on impact";
+	private Rigidbody2D rb;
+	private string targetTag = "Enemy";
+
+	// ability parameter 
+	public const float shockDuration = 0.5f;
+	public const float baseDamage = 1f;
+	public float speed = 20f;
+
+	private Stopwatch durationTimer = new Stopwatch();
+
+	void Start () {
+		rb = GetComponent <Rigidbody2D> ();
+		rb.velocity = transform.up * speed;
+		Destroy (gameObject, 3f);
+	}
+
+	public LightningSpell(): base(new BasicObjectInfo (aName, aDescription))
+	{
+		this.AbilityBehaviors.Add (new Shock (2f, shockDuration, baseDamage));
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag (targetTag)) {
+			other.gameObject.GetComponent<EnemyScript>().hitPoints -= baseDamage;
+			other.gameObject.GetComponent<EnemyScript> ().speed = 0f;
+
+			StartCoroutine (STUN(other.gameObject));
+
+		}	
+
+	}
+
+	private IEnumerator STUN(GameObject objectHit)
+	{
+		
+		yield return new WaitForSeconds (shockDuration);
+		objectHit.GetComponent<EnemyScript> ().speed = 3f;
+		Destroy (gameObject);
+		yield return null;
+
+	}
+}
